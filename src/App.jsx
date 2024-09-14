b7df3c234650e636d8c6d1cf98c23cbe83e1d6da
malik rasheed <codedev366@gmail.com>
2024-09-14 17:33:39 +0500

    files are added

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Appe from "./Firebase";
+import UserAuth from "./SignUp";
+// import SignIn from "./SignIn";
+import { getDatabase, ref, set } from "firebase/database";
 
-function App() {
-  const [count, setCount] = useState(0)
+const db = getDatabase(Appe);
 
+const App = () => {
+  const WriteUserData = () => {
+    console.log("clicked");
+
+    set(ref(db, "users/rasheed"), {
+      name: "rasheed",
+      age: 24,
+      profession: "software engineer",
+    })
+      .then(() => {
+        console.log("Data written successfully!");
+      })
+      .catch((error) => {
+        console.error("error writing ", error);
+      });
+  };
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+    <div>
+      {/* <button onClick={WriteUserData}>click me</button> */}
+      <UserAuth />
+      {/* <SignIn /> */}
+    </div>
+  );
+};
+export default App;
